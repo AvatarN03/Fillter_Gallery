8c0c0432c697f58fa6d12decdcbf2bec0a6f4d26
Prashanth Naidu <114817400+AvatarN03@users.noreply.github.com>
2024-06-28 14:21:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+(function(){
+    const buttons = document.querySelectorAll('.btn')
+    const storeImages = document.querySelectorAll('.store_item');
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click',(e)=>{
+                e.preventDefault()
+                console.log(e.target);
+                const filter = e.target.dataset.filter;
+                console.log(filter);
+
+                storeImages.forEach((item)=>{
+                    if(filter === 'all')
+                    {
+                        item.style.display = 'block'
+                    }
+                    else{
+                        if(item.classList.contains(filter)){
+                            item.style.display = 'block';
+                        }
+                        else{
+                            item.style.display = 'none';
+                        }
+                    }
+                })
+        })
+    })
+})();
